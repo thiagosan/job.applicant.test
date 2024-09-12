ad06d632e7ad13f8754df4d03e91b64f46f73d36
Eloí Oliveira <eloi.antonio.oliveira@gmail.com>
2024-09-12 10:10:11 -0300

    update API project to dotnet 8

--- BestHB/Program.cs
+++ BestHB/Program.cs
@@ -1,24 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+﻿using BestHB.Domain.Repositories;
+using BestHB.Domain.Service;
+using BestHB.Domain.Services;
+using BestHB.Repository;
 
-namespace BestHB
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddTransient<IOrderService, OrderService>();
+builder.Services.AddTransient<IRepository, InstrumentInfoRepository>();
+builder.Services.AddTransient<IRepository, OrderRepository>();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateWebHostBuilder(args).Build().Run();
-        }
-
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
-    }
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
