c3958b6d0b744f0ec6deda776f097befe2d02b97
Eloí Oliveira <eloi.antonio.oliveira@gmail.com>
2024-09-25 11:49:38 +0000

    remove swagger, https and auth config from application startup

--- BestHB/Program.cs
+++ BestHB/Program.cs
@@ -11,22 +11,8 @@ builder.Services.AddTransient<IRepository, InstrumentInfoRepository>();
 builder.Services.AddTransient<IRepository, OrderRepository>();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
 
 app.MapControllers();
 
